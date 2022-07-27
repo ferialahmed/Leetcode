414934131f51fd2e23e8b1bff7a79be89b3f7c37
ferialahmed <63975253+ferialahmed@users.noreply.github.com>
2022-07-27 18:39:21 +0200

    Time: 132 ms (56.89%), Space: 48.5 MB (97.76%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -5,13 +5,5 @@
 var reverseString = function(s) {
     s.reverse();
     return s;
-    // let leftPointer = 0;
-    // let rightPointer = s.length - 1;
-    // while(leftPointer < rightPointer)
-    //     {
-    //         [s[leftPointer], s[rightPointer]] = [s[rightPointer], s[leftPointer]];
-    //         leftPointer ++;
-    //         rightPointer --;
-    //     }
-    // return s;
+
 };
