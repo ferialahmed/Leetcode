ea97095338f39cb1ae90bc3111489e7b45a9ee0e
ferialahmed <63975253+ferialahmed@users.noreply.github.com>
2022-07-30 17:45:34 +0200

    Time: 81 ms (95.58%), Space: 44.9 MB (79.61%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    let maximum = 0, res = 0, area = 0, sr = 0, sc = 0;
+    const rows = grid.length;
+    const cols = grid[0].length;
+    // for (let sr =0; sr< rows; sr++)
+    while (sr < rows)
+        {
+            // for (let sc = 0; sc < cols; sc++)
+            while (sc < cols)
+                {
+                    if (grid[sr][sc] === 1)
+                        {
+                            area = 0;
+                            res = Max(sr,sc);
+                            maximum = Math.max(res, maximum);
+                        }
+                    sc ++;
+                }
+            sr ++;
+            sc = 0;
+        }
+    function Max (r, c)
+    {
+        if (r === rows || r < 0 || c === cols || c < 0) return;
+        if (grid[r][c] !== 1) return;
+        grid[r][c] = 0;
+        area ++;
+        Max(r+1, c); Max(r-1, c);
+        Max(r, c+1); Max (r, c-1);
+        return area;
+    }
+    return maximum;
+};
