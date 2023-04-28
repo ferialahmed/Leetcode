918e3bd1a5b6600c0e0af859bd8258b0cd0d35b5
Ferial Ahmed <63975253+ferialahmed@users.noreply.github.com>
2023-04-28 22:54:17 +0200

    Add another solution

--- 1299. Replace Elements with Greatest Element on Right Side/1299. Replace Elements with Greatest Element on Right Side.js
+++ 1299. Replace Elements with Greatest Element on Right Side/1299. Replace Elements with Greatest Element on Right Side.js
@@ -2,17 +2,38 @@
  * @param {number[]} arr
  * @return {number[]}
  */
+// var replaceElements = function(arr) {
+    
+//     let max = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = i+1; j < arr.length; j++){
+//             if (arr[j] > max){
+//                 max = arr[j]
+//             }
+//         }
+//         arr[i] = max;
+//         max = 0;
+//     }
+//     arr[arr.length - 1] = -1;
+//     return arr;
+// };
+
+
+//Add another solution with better time complexity
 var replaceElements = function(arr) {
     
     let max = 0;
     for(let i = 0; i < arr.length; i++){
-        for(let j = i+1; j < arr.length; j++){
-            if (arr[j] > max){
-                max = arr[j]
+        if (max === 0 || max === arr[i]){
+            max = 0;
+            for(let j = i+1; j < arr.length; j++){
+                if (arr[j] > max){
+                    max = arr[j]
+                }
             }
         }
         arr[i] = max;
-        max = 0;
+        
     }
     arr[arr.length - 1] = -1;
     return arr;
